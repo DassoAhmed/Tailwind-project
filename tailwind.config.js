101c06c7b287c0afe5e4b72b9d47dd95abe57e65
DassoAhmed <dassoahmed@gmail.com>
2022-03-16 16:54:54 +0100

    updates index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'title': ['Sansita', 'sans-serif'],
-        'body' : ['Lato','sans-serif']
+        title: ["Sansita", "sans-serif"],
+        body: ["Lato", "sans-serif"],
       },
       colors: {
-        'pri': '#081F4D',
-        'sec': '#0083FF',
-        'gen': '#3B4D71',
-        'our-bg': '#D9E6FF'
-      }
+        pri: "#081F4D",
+        sec: "#0083FF",
+        gen: "#3B4D71",
+        "our-bg": "#D9E6FF",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
